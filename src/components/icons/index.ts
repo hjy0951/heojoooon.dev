aebd6625804f81ff3bfef7c8149a5a847e073931
hjy0951 <hjy0951@gmail.com>
2025-02-05 16:50:20 +0900

    feat: 게시글 리스트에 들어갈 아이템 컴포넌트 재구현

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,4 @@ export * from "./check-box";
 export * from "./light-bulb";
 export * from "./warning";
 export * from "./star";
+export * from "./calendar";
---
+++ src/components/post/list-item.tsx
@@ -0,0 +1,93 @@
+import { css, cx } from "#/styled-system/css";
+import { pretendard } from "@/styles/font";
+import Image from "next/image";
+import Link from "next/link";
+import { CalendarIcon } from "../icons";
+
+type PostListItemProps = {
+  title: string;
+  description: string;
+  createdAt: string;
+  slug: string;
+};
+
+export const PostListItem = ({
+  title,
+  description,
+  createdAt,
+  slug,
+}: PostListItemProps) => {
+  return (
+    <Link href={`/posts/${slug}`} className={containerStyle}>
+      <div className={cx(coverImageWrawpperStyle, coverImageStyle)}>
+        <Image
+          className={coverImageStyle}
+          src={`/contents/${slug}/cover.png`}
+          alt={`"${title}" 커버 이미지`}
+          fill
+          priority
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      <div className={cx(postInfoStyle, pretendard.className)}>
+        <div>
+          <p className={titleStyle}>{title}</p>
+          <p className={descriptionStyle}>{description}</p>
+        </div>
+
+        <div className={dateWrapperStyle}>
+          <CalendarIcon />
+          <p className={dateStyle}>{createdAt}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+const containerStyle = css({
+  display: "flex",
+  gap: "12px",
+  maxWidth: "440px",
+  borderRadius: "12px",
+  transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+
+  _hover: {
+    boxShadow: "0 4px 6px rgba(0,0,0,0.40), 0 2px 4px rgba(0,0,0,0.32)",
+    transform: "translateY(-4px)",
+  },
+});
+
+const coverImageWrawpperStyle = css({
+  position: "relative",
+  width: "180px",
+  height: "180px",
+  minWidth: "180px",
+});
+
+const coverImageStyle = css({
+  borderRadius: "6px",
+});
+
+const postInfoStyle = css({
+  flex: 1,
+  p: "12px",
+  display: "flex",
+  flexDir: "column",
+  justifyContent: "space-between",
+});
+
+const titleStyle = css({ fontWeight: 600 });
+
+const descriptionStyle = css({ fontSize: "14px" });
+
+const dateWrapperStyle = css({
+  display: "flex",
+  gap: "4px",
+  alignItems: "center",
+  justifyContent: "end",
+});
+
+const dateStyle = css({
+  fontSize: "14px",
+});
