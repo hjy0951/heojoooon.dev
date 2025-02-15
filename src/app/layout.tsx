8da052799cbfd7e660ed5a4c7189e728af368086
hjy0951 <hjy0951@gmail.com>
2025-02-15 16:01:39 +0900

    chore: 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,8 @@ import "@/styles/globals.css";
 import { Header, Footer } from "@/components/layout";
 
 export const metadata: Metadata = {
-  title: "프론트엔드 개발자 허준영입니다.",
-  description: "별 세개짜리 개발자",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  title: "별 세 개짜리 개발자",
+  description: "프론트엔드 개발자 허준영입니다.",
 };
 
 export default function RootLayout({
