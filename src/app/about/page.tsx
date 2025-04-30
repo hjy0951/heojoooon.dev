0c29da20f80258b5523e07349794f41fd89f3e0c
hjy0951 <hjy0951@gmail.com>
2025-04-30 22:24:00 +0900

    design: 불필요한 이미지 제거 및 about 페이지 초안 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,31 @@
-import Image from "next/image";
-import ConstructionImage from "@/assets/images/construction.png";
 import { css, cx } from "#/styled-system/css";
+import { Section } from "@/components/about";
+import { ProfileCard } from "@/components/layout";
 import { pretendard } from "@/styles/font";
 
 const AboutPage = () => {
   return (
     <div className={cx(pretendard.className, containerStyle)}>
-      <Image
-        src={ConstructionImage}
-        alt="construction icon"
-        className={imageStyle}
-      />
+      <ProfileCard isViewName={false} />
 
-      <p className={descriptionStyle}>🚧 공사중인 페이지입니다.</p>
+      <Section title="ABOUT ME">
+        <p className={textStyle}>안녕하세요. 프론트엔드 개발자 허준영입니다.</p>
+
+        <p className={textStyle}>
+          팀원들과의 원활한 협업을 위해 항상 먼저 소통하려 노력하며, 함께 문제를
+          해결하고 나아가는 과정을 중요하게 생각합니다. 단순히 의견을 나누는
+          것을 넘어, 서로의 입장을 이해하고 더 나은 방향을 함께 고민하는
+          개발자가 되고자 합니다. 협업은 결과 이상의 가치를 만들어낸다고 믿기에,
+          언제나 열린 자세로 팀에 기여하려 노력하고 있습니다.
+        </p>
+
+        <p className={textStyle}>
+          내가 만든 서비스가 사용자와 상호작용하며 실제로 동작하는 모습을 보며,
+          단순한 개발을 넘어 사용자 경험에 가치를 더할 수 있다는 점에 큰 매력과
+          보람을 느껴 시스템 엔지니어링에서 프론트엔드 개발로 전환하게
+          되었습니다.
+        </p>
+      </Section>
     </div>
   );
 };
@@ -20,20 +33,16 @@ const AboutPage = () => {
 export default AboutPage;
 
 const containerStyle = css({
-  my: "60px",
+  margin: "auto",
+  padding: "32px",
+  maxWidth: "800px",
   width: "100%",
   display: "flex",
   flexDir: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "48px",
 });
 
-const imageStyle = css({
-  width: "30%",
-});
-
-const descriptionStyle = css({
-  fontSize: "20px",
-  fontWeight: 600,
+const textStyle = css({
+  fontSize: "15px",
+  fontWeight: 400,
+  color: "#202125",
 });
---
+++ src/components/about/section.tsx
@@ -0,0 +1,41 @@
+import { css } from "#/styled-system/css";
+import { PropsWithChildren } from "react";
+
+interface SectionProps {
+  title: string;
+}
+
+export const Section = ({
+  title,
+  children,
+}: PropsWithChildren<SectionProps>) => {
+  return (
+    <section className={containerStyle}>
+      {/* header */}
+      <div>
+        <div className={dividerStyle} />
+        <span className={titleStyle}>{title}</span>
+      </div>
+
+      {children}
+    </section>
+  );
+};
+
+const containerStyle = css({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+});
+
+const dividerStyle = css({
+  width: "80px",
+  height: "4px",
+  backgroundColor: "black",
+});
+
+const titleStyle = css({
+  fontSize: 24,
+  fontWeight: 700,
+});
