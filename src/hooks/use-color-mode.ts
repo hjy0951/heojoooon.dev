46a2b4dc91865b17391dcda1c629ff99e6125d88
hjy0951 <hjy0951@gmail.com>
2025-06-10 01:41:33 +0900

    chore: 주석 제거

--- src/hooks/use-color-mode.ts
+++ src/hooks/use-color-mode.ts
@@ -1,41 +1,5 @@
-// import { useLayoutEffect, useState } from "react";
-
 import { ColorModeContext } from "@/contexts/color-mode";
 import { useContext } from "react";
-
-// export const COLOR_MODES = ["light", "dark"] as const;
-// export type ColorModeType = (typeof COLOR_MODES)[number];
-
-// export const useColorMode = () => {
-//   const [colorMode, setColorMode] = useState<ColorModeType>("light");
-
-//   const toggleColorMode = () => {
-//     const nextMode = colorMode === "dark" ? "light" : "dark";
-//     if (typeof document !== "undefined") {
-//       document.cookie = `color-mode=${nextMode}`;
-//     }
-//     setColorMode(nextMode);
-//   };
-
-//   useLayoutEffect(() => {
-//     const currentMode = getColorModeFromCookie() || "light";
-//     setColorMode(currentMode as ColorModeType);
-//   }, []);
-
-//   useLayoutEffect(() => {
-//     if (typeof document !== "undefined") {
-//       document.documentElement.setAttribute("data-color-mode", colorMode);
-//     }
-//   }, [colorMode]);
-
-//   return { colorMode, toggleColorMode };
-// };
-
-// function getColorModeFromCookie(): string | undefined {
-//   if (typeof document === "undefined") return undefined;
-//   const match = document.cookie.match(/(?:^|;\s*)color-mode=([^;]*)/);
-//   return match ? decodeURIComponent(match[1]) : undefined;
-// }
 
 export const useColorMode = () => {
   const context = useContext(ColorModeContext);
